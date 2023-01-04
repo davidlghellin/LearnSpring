63a39659daf99680a0d7108dba5b63f177c9b161
David López <davidlghellin@gmail.com>
2023-01-04 14:48:07 +0100

    [SPRING-BOOT] [DEVCONTAINER] Spring con JPA simple con bbdd en memoria (video03)

---
+++ learn_001_spring/README.md
@@ -0,0 +1,7 @@
+# SpringJPA
+
+Para poder ejecutar este ejemplo básico
+
+```sh
+mvn clean compile exec:java -Dexec.mainClass="es.david.ObSprindatajpaApplication"
+```
---
+++ learn_001_spring/src/main/java/es/david/ObSprindatajpaApplication.java
@@ -0,0 +1,29 @@
+package es.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ObSprindatajpaApplication {
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(ObSprindatajpaApplication.class, args);
+
+        CocheRepository repository = context.getBean(CocheRepository.class);
+
+        System.out.println("El num de coches en base de datos es: " + repository.count());
+
+        // crear y almacenar un coche en base de datos
+        Coche toyota = new Coche(null, "Toyota", "Prius", 2010);
+        repository.save(toyota);
+        Coche seat = new Coche(null, "Seat", "Panda", 2000);
+
+        repository.save(seat);
+
+        System.out.println("El num de coches en base de datos es: " + repository.count());
+
+        // recuperar todos
+        System.out.println(repository.findAll());
+    }
+}
