33605b703581d9e2585a4d19406b6e9b8c65338c
David López <davidlghellin@gmail.com>
2023-01-28 18:47:38 +0100

    [SPRING-BOOT] [WEB] [H2] [DEVCONTAINER] [SECURITY] Ejemplo básico de seguridad y cookies

---
+++ learn_003_spring_security/README.md
@@ -0,0 +1,11 @@
+# Security
+
+```sh
+http -a user:51a1151c-f747-4c17-870d-e932d0134a8f get 127.0.0.1:8080/hola
+curl -u user:51a1151c-f747-4c17-870d-e932d0134a8f -i  127.0.0.1:8080/hola
+```
+
+```sh
+http -a david:1234 get 127.0.0.1:8080/bootstrap
+curl -u david:1234 -i  127.0.0.1:8080/bootstrap
+```
---
+++ learn_003_spring_security/src/main/java/es/david/obrestdatajpa/HolaController.java
@@ -0,0 +1,26 @@
+package es.david.obrestdatajpa;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @Value("${app.message}")
+    String message;
+
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        System.out.println(message);
+        return "Hola mundo\n";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap(){
+        // no se usa el html. Usariamos por ejemplo el spring mvc
+        return "<h1>Prueba</h1>";
+    }
+
+}
