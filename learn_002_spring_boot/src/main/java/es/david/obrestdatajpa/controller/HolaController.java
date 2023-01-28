aefbe41bb67d4ae30b4df052aceed38b11fa23f5
David López <davidlghellin@gmail.com>
2023-01-28 11:29:55 +0100

    [SPRING-BOOT] [WEB] [H2] [DEVCONTAINER] Añadimos variable para leer desde properties

--- learn_002_spring_boot/src/main/java/es/david/obrestdatajpa/controller/HolaController.java
+++ learn_002_spring_boot/src/main/java/es/david/obrestdatajpa/controller/HolaController.java
@@ -1,13 +1,19 @@
 package es.david.obrestdatajpa.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaController {
 
+    @Value("${app.message}")
+    String message;
+
+
     @GetMapping("/hola")
     public String holaMundo() {
+        System.out.println(message);
         return "Hola mundo\n";
     }
 
